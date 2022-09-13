3d9153b63a82631aed32a423b5fdd59d4ce59251
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-13 21:34:43 +0300

    notes25

---
+++ runestone/notes25.java
@@ -0,0 +1,17 @@
+public class notes25 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("2.5 notlari");
+    }
+    
+}
+
+/*
+ * if a method is defined like void methodName() then it does not return anything (returns void)
+ * 
+ * however, some methods return some data whose type is defined when the method is programmed
+ * 
+ * .toString method can be called on a lot of objects to convert them to a string that makes
+ * them easier to process in most scenarios
+ */

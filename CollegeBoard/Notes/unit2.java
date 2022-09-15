dff770219cc451ceb83b3f67ec83e3888f95d12e
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-15 21:45:22 +0300

    collegeboard unit 2 notes

---
+++ CollegeBoard/Notes/unit2.java
@@ -0,0 +1,18 @@
+public class unit2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Unit 2 notes");
+    } 
+}
+
+/*
+ * When multiple variables refer to the same instance, a modification on one of the variables will 
+ * effect the same object with other variables causing the object to change for all references
+ * 
+ * constructors with different types of variables or different amount of variables can be created 
+ * using a concept called overloading. This allows us to create instances of the same class with default
+ * values or with some values missing.
+ * 
+ * when an instance with no value is created, the reference type refers to null in the memory
+ */

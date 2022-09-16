8d108f800d42be9c1e37ac60e1324a400dbcfd9f
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-16 22:11:44 +0300

    notes26 eklendi

---
+++ runestone/notes26.java
@@ -0,0 +1,22 @@
+public class notes26 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("2.6 notlar");
+    }  
+}
+/*
+ * When you try to print a variable with the value null it prints null. (Strings can be assigned null)
+ * 
+ * Assigning null to a string shows that it does not refer to any object yet
+ * 
+ * String strname new String("content") is also a valid way to create a string
+ * 
+ * String strname = "content" -> this usage is called string literals  
+ * 
+ * Class of a string object is String, the class of a String is Object. Interesting
+ * 
+ * Every instance of a string inherits some methods and fields from the String class.
+ * 
+ * Every instance in java will at some point reach the Object class (highest class in Java class order)
+ */

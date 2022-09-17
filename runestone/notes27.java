6328078a6e899235766245f4cb130e74c669ef09
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-17 15:40:57 +0300

    notes 27 runestone

---
+++ runestone/notes27.java
@@ -0,0 +1,27 @@
+public class notes27 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("notes 27");
+    }    
+}
+
+/*
+ * java is 0 indexed and the last index of an iterable object is -1
+ * 
+ * indexOf(str) method returns the index of a string within a string, -1 if it doesnt exist
+ * 
+ * substring(int, int) returns a new string from the specified indices but -1 the last index
+ * if only one int is specified it goes from index to end, without the -1 this time
+ * 
+ * compareTo(str) and equals(str) are case sensitive methods that compare two strings
+ * 
+ * the total alphabetical placement value determines the return value of compareTo
+ * 
+ * If the alphabetical placement value of the argument is lower it returns positive, 0 if same and negative 
+ * if higher
+ * 
+ * equals only returns true if two strings are exactly equal, else false
+ * 
+ * 
+ */

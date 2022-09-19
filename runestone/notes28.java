327d3529dfc07af9d6a10a7aef4a11d0bf63be21
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-19 20:25:03 +0300

    runestone 28

---
+++ runestone/notes28.java
@@ -0,0 +1,26 @@
+public class notes28 { 
+
+    public static void main(String[] args) {
+        
+        System.out.println("notes 28");
+    }    
+}
+
+/*
+ * wrapper classes are surrounding classes for primitive data types that provide methods
+ * that you can use for the respective data types
+ * 
+ * In java 9+ you can just say Integer / Double i = val but in the exam 
+ * the following syntax is needed:
+ * 
+ * Integer / Double i = new Integer / Double (i)
+ * 
+ * autoboxing happens when a method is called upon a primitve type instance that expects
+ * an object. For example, int is converted to Integer. The reverse process is called 
+ * unboxing
+ * 
+ * A common usage example for a wrapper class is the parseInt / parseDouble methods that are
+ * in their respective wrapper clases (Integer and Double)
+ * 
+ * 
+ */

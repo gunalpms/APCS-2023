59338d21c1afdb963b93581b0c5c634cf883041f
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-19 21:07:37 +0300

    notes 31

---
+++ runestone/notes31.java
@@ -0,0 +1,13 @@
+public class notes31 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("notes 31");
+    }
+}
+
+/*
+ * Two seperate instances of the same class do not return true when 
+ * the == operator is used because they are not of the same reference
+ * 
+ */

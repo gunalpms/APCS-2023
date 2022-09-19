065c9a51e2c5ba40a0634d3766ecd2f8c1ca0a40
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-19 20:13:14 +0300

    collegeboard 23-25

---
+++ CollegeBoard/Notes/unit23_25.java
@@ -0,0 +1,22 @@
+public class unit23_25 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("collegeboard 2.3 2.5 arasi notlar");
+    }  
+}
+/*
+ * a method signature is a method definition (parameters and tasks)
+ * 
+ * when an object name is referencing to null, calling a method on said object will create a
+ * null pointer exception 
+ * 
+ * method overloading is when a method with the same name has multiple definitions with 
+ * a different amount of parameters and/or different data types for the parameters
+ * 
+ * when a method has a return type the result of the method can be stored in a variable or be used
+ * in an expression
+ * 
+ * methods with return types must return a value with the same type 
+ * 
+ */

4ac1fc60dab4661d4766fe39a95584ff81cfe6fa
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-21 21:28:45 +0300

    olmayan oracle 31 odevi eklendi

---
+++ ORACLE/variables31.java
@@ -0,0 +1,12 @@
+public class variables31 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("odev nerede?");
+
+        // Exercise 2 icin gereken java dosyasina ulasamiyorum o yuzden bos
+        // Exercise 3 icin gereken java dosyasi da maalesef yok o yuzden yine bos
+        // Acikcasi eksik odev yapmak istemistim ancak ortada yapabilecegim bir sey ne yazik ki yok
+    }
+    
+}

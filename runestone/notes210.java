5cd59bbecb4e19c5831c179710a49fdee7e9dd9a
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-22 21:41:07 +0300

    notes 210

---
+++ runestone/notes210.java
@@ -0,0 +1,13 @@
+public class notes210 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("notes 2.10");
+    }
+}
+
+/*
+ * In this unit mostly classes and their behaviours were covered
+ * 
+ * Videoda açıkladım
+ */

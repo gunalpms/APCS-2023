cefa208879c4e974ac0dc7b37d2558f53317465e
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-22 21:11:06 +0300

    cb2627

---
+++ CollegeBoard/Notes/unit26_27.java
@@ -0,0 +1,15 @@
+public class unit26_27 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Notes 2.6 and 2.7");
+    }
+}
+
+/*
+ * String objects are immutable. Therefore, string methods cannot modify a string instance
+ * 
+ * We can use the + operator to concatenate (verb to describe combining data) strings
+ * 
+ * 
+ */

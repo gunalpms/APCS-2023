94f8b302c833b34eff89fab242028f51b9b073f7
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-23 21:38:43 +0300

    china pilot

--- EXIT_CARD/chinaPilot.java
+++ EXIT_CARD/chinaPilot.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 public class chinaPilot {
 
     public static void main(String[] args) {
-        chinaPilotCalc c =  new chinaPilotCalc("allah muhammed");
-        // SAYI ARRAYI AL
-
+    
+        chinaPilotCalc c =  new chinaPilotCalc("x"); // kotu koddan oturu string ile obje yaratip 
+        // constructordan islem gerceklestiriliyor
     }
 
     public static class chinaPilotCalc{
+        
 
         public chinaPilotCalc() {
 
@@ -15,29 +18,57 @@ public class chinaPilot {
 
         public chinaPilotCalc(String a) {
 
-            a = a.toLowerCase();
+            Scanner s = new Scanner(System.in);
+            String b = s.nextLine(); // string komutu al
 
-            String[] args = a.split(" ");
+            b = b.toLowerCase();
 
-            String tamsayi = args[0];
+            String[] args = b.split(" ");
+
+            String tamsayi = args[0]; // ilk komut kac sayi ile islem yapilacagi
+            String islem = args[2]; // ikinci komut ise islemin ne oldugu
+
+            double tamsayi_d = 0;
+            // ilk komutu stringden sayisal degere ceviriyoruz
+            if (tamsayi.equals("bir") ) {tamsayi_d = 1;}
+            if (tamsayi.equals("iki")) {tamsayi_d = 2;}
+            if (tamsayi.equals("uc")) {tamsayi_d = 3;}
+            if (tamsayi.equals("dort")) {tamsayi_d = 4;}
+            if (tamsayi.equals("bes")) {tamsayi_d = 5;}
+
+            // ikinci komuta gore islemler yapiliyor
+            // loop hepsinde ayni mantigini videoda acikliyorum
+            if (islem.equals("bol")) {
+                double x = s.nextDouble();
+                for (int i = 1; i < tamsayi_d; i++) {
+                    double y = s.nextDouble();
+                    x /= y; }
+                s.close();
+                System.out.println(x); }
             
-            String islem = args[2];
-
-            double tamsayi_int = 0;
-
-            if (tamsayi == "bir") {tamsayi_int = 1;}
-            if (tamsayi == "iki") {tamsayi_int = 2;}
-            if (tamsayi == "uc") {tamsayi_int = 3;}
-            if (tamsayi == "dort") {tamsayi_int = 4;}
-            if (tamsayi == "bes") {tamsayi_int = 5;}
-            
-            if (islem == "bolme") { 
-                for (int i = 1; i < tamsayi_int +1; i++) {
+            if (islem.equals("carp")) {
+                double x = s.nextDouble();
+                for (int i = 1; i < tamsayi_d; i++) {
+                    double y = s.nextDouble();
+                    x *= y; }
+                s.close();
+                System.out.println(x); }
                     
-                }
-            }
-
-
+            if (islem.equals("topla")) {
+                double x = s.nextDouble();
+                for (int i = 1; i < tamsayi_d; i++) {
+                    double y = s.nextDouble();
+                    x += y; }
+                s.close();
+                System.out.println(x); }
+            
+            if (islem.equals("cikart")) {
+                double x = s.nextDouble();
+                for (int i = 1; i < tamsayi_d; i++) {
+                    double y = s.nextDouble();
+                    x -= y; }
+                s.close();
+                System.out.println(x); }
         }
     }    
 }

b245dfeb4e645181347abdb4beb1966ff549c75b
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-09-26 20:48:24 +0300

    notes 28 29

---
+++ CollegeBoard/Notes/unit28_29.java
@@ -0,0 +1,20 @@
+public class unit28_29 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("notes 28-29");
+    }    
+}
+
+/*
+ * These wrapper classes are apparently useful with array lists(?) but alone I have not seen a purpose
+ * 
+ * Autoboxing and unboxing processes are done automatically by the compiler
+ * 
+ * The math class is static which means that they belong to a class, not an object
+ * 
+ * Therefore they do not need an instance variable to run
+ * 
+ * Math.random is not inclusive such that the function itself never returns 1. (returns limit)
+ *
+ */

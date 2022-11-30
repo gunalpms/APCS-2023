c9968896f4d602ea17d4f0957515cce9eaf55d4c
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-11-30 18:05:12 +0300

    comments eklendi

--- Zor Sorular/pet ve quadratic/Pet.java
+++ Zor Sorular/pet ve quadratic/Pet.java
@@ -1,12 +1,12 @@
 public class Pet {
     private String name;
     private String type;
-    private static int numCats;
+    private static int numCats; // kedi kopek ve diger tur sayisi class ozelligi oldugundan static tuttum
     private static int numDogs;
     private static int numOtherPets;
 
     public Pet(String n, String t) {
-        t = t.toLowerCase();
+        t = t.toLowerCase(); // case olaylarindan hic hoslanmam
         name = n;
         type = t;
 
@@ -14,20 +14,21 @@ public class Pet {
         else if (t.equals("dog")) {numDogs++;}
         else {numOtherPets++;}
     }
-
+    // alttaki 3 fonksiyon instancelarla isi olmadigi icin staticler
     public static void Greeting() {
         System.out.println("Welcome to the pet object class (?)");
     }
 
-    private static int totalNumPets() {
+    private static int totalNumPets() { // toplam evcil hayvan sayisini verir
         return numCats + numDogs + numOtherPets;
     }
 
-    public static void printPetNumbers() {
+    public static void printPetNumbers() { // her turden ne kadar oldugunu ve toplam ne kadar hayvan oldugunu terminale yazar
         System.out.println("Number of dogs: " + numDogs + 
         " Number of cats: " + numCats + " Number of other pets: " + numOtherPets
         + " Total number of pets: " + totalNumPets());
     }
+    // her bir pet objesi icin ismi, turu ve o turden kac uyesinin oldugunu terminale yazan bir fonksiyon
     public void printTypeInfo() {
         System.out.print("The name of this pet is " + name + ", ");
         if (type.equals("cat")) 
--- Zor Sorular/pet ve quadratic/Quadratic.java
+++ Zor Sorular/pet ve quadratic/Quadratic.java
@@ -3,14 +3,14 @@ public class Quadratic {
     private double b;
     private double c;
 
-    public Quadratic(double x, double y, double z) {
+    public Quadratic(double x, double y, double z) { // x y z demek ne kadar dogru bilemedim ama is goruyor 
         a = x;
         b = y;
         c = z;
     }
 
     public String solveEquation() {
-        double d = (Math.pow(b, 2)) - (4*a*c);
+        double d = (Math.pow(b, 2)) - (4*a*c); // discriminant bu sekilde yazmak koda aciklik kazandiriyor kanimca
         double x1 = ((-1*b)-Math.sqrt(d))/(2*a);
         double x2 = ((-1*b)+Math.sqrt(d))/(2*a);
         return ("The roots are " + x1 + " and " + x2);
